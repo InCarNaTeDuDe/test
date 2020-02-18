94c9fd4cf003441b2cd74103878f4f2a0b8ff6d0
bharath_maska <20809168+InCarNaTeDuDe@users.noreply.github.com>
2020-02-18 20:25:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,9 +4,19 @@ const resourcesToCache = [
     '/', '/index.html'
 ];
 
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(resourcesToCache);
+            })
+    );
+});
+
 /* Implementing Cache first Strategy */
 self.addEventListener('fetch', event => {
-    event.respondWith(caches.match(even.request).then(cachedItem => {
+    event.respondWith(caches.match(event.request).then(cachedItem => {
         return cachedItem || fetch(event.request);
     }));
-})
+});
