1ec194e66b5d0f599305688c455b0f31b37f49d7
bharath_maska <20809168+InCarNaTeDuDe@users.noreply.github.com>
2020-03-12 13:07:02 +0530

    server side re-direction to https

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const express = require('express'),
 
 
 app.use(express.static(path.join(__dirname, '/')));
+if (process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+        if (req.header('x-forwarded-proto') !== 'https') {
+            res.redirect(`https://${req.header('host')}${req.url}`);
+        } else {
+            next();
+        }
+    })
+}
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname });
 });
